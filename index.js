c7fcfcad6e10ccc0563b43901e5e7d572d6909b7
Florian Wolff <florian.wolff@pinuts.de>
2019-03-28 15:32:19 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+            modules: true,
+        },
+        ecmaVersion: 6,
+        sourceType: 'module',
+    },
+    root: true,
+    rules: {
+        indent: ['error', 4, { SwitchCase: 1 }],
+        'no-console': 0,
+        'no-var': 'error',
+        'no-undef': 'error',
+        'lines-between-class-members': ['error', 'always'],
+        'padding-line-between-statements': [
+            'error',
+            { blankLine: 'always', prev: 'block', next: '*' },
+            { blankLine: 'always', prev: 'block-like', next: '*' },
+            { blankLine: 'always', prev: 'multiline-block-like', next: '*' },
+            { blankLine: 'always', prev: 'multiline-expression', next: '*' },
+            { blankLine: 'always', prev: '*', next: 'return' },
+        ],
+        'prefer-const': 'error',
+        'prefer-destructuring': 'error',
+        quotes: ['error', 'single'],
+    },
+};
