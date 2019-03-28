194439663f1ade09c9c64157d8ce84512f1162e5
Florian Wolff <florian.wolff@pinuts.de>
2019-03-28 16:40:13 +0100

    eslint:recommended

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ module.exports = {
         es6: true,
         node: true,
     },
+    extends: [
+        'eslint:recommended'
+    ],
     parserOptions: {
         ecmaFeatures: {
             jsx: true,
